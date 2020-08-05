a078b81d8e1228fce0546e274df2cb23fbbca3c1
Michael Karcher <michael.d.karcher@gmail.com>
2020-08-04 21:36:46 -0700

    Updating variable names in test_vbsupertree.

--- tests/test_vbsupertree.py
+++ tests/test_vbsupertree.py
@@ -120,55 +120,55 @@ class TestSBN(unittest.TestCase):
             PCSP(Subsplit("A", "BC"), Subsplit("B", "C")): 1.0,
             PCSP(Subsplit("AB", "CD"), Subsplit("C", "D")): 1.0,
         }
-        scd = SBN(pcsp_conditional_probs)
-        scd.normalize()
+        sbn = SBN(pcsp_conditional_probs)
+        sbn.normalize()
         tree = MyTree("((A,(B,C)),D);")
-        self.assertAlmostEqual(scd.likelihood(tree), 0.2)
-        self.assertAlmostEqual(scd.log_likelihood(tree), math.log(0.2))
+        self.assertAlmostEqual(sbn.likelihood(tree), 0.2)
+        self.assertAlmostEqual(sbn.log_likelihood(tree), math.log(0.2))
 
-    def test_random_scd(self):
-        scd = SBN.random("ABCDE")
-        self.assertEqual(len(scd), 210)
-        tree_dist = scd.tree_distribution()
+    def test_random_sbn(self):
+        sbn = SBN.random("ABCDE")
+        self.assertEqual(len(sbn), 210)
+        tree_dist = sbn.tree_distribution()
         self.assertEqual(len(tree_dist), 105)
         tree, prob = random.choice(list(tree_dist.items()))
-        lik = scd.likelihood(tree)
+        lik = sbn.likelihood(tree)
         self.assertAlmostEqual(prob, lik)
 
     def test_tree_distribution(self):
-        scd = SBN.random_with_sparsity("ABCDEF", 0.5)
-        tree_dist = scd.tree_distribution()
+        sbn = SBN.random_with_sparsity("ABCDEF", 0.5)
+        tree_dist = sbn.tree_distribution()
         tree, prob = random.choice(list(tree_dist.items()))
-        lik = scd.likelihood(tree)
+        lik = sbn.likelihood(tree)
         self.assertAlmostEqual(prob, lik)
 
     def test_subsplit_probabilities(self):
-        scd = SBN.random("ABCDE")
-        tree_dist = scd.tree_distribution()
-        subsplit_probs = scd.subsplit_probabilities()
+        sbn = SBN.random("ABCDE")
+        tree_dist = sbn.tree_distribution()
+        subsplit_probs = sbn.subsplit_probabilities()
         subsplit, prob = random.choice(list(subsplit_probs.items()))
         self.assertAlmostEqual(tree_dist.feature_prob(subsplit), prob)
 
     def test_clade_and_subsplit_probabilities(self):
-        scd = SBN.random("ABCDE")
-        tree_dist = scd.tree_distribution()
+        sbn = SBN.random("ABCDE")
+        tree_dist = sbn.tree_distribution()
 
-        clade_probs = scd.clade_probabilities()
+        clade_probs = sbn.clade_probabilities()
 
         clade = random.choice(list(clade_probs.keys()))
         self.assertAlmostEqual(tree_dist.feature_prob(clade), clade_probs.get(clade, 0.0))
 
     def test_pcsp_probabilities(self):
-        scd = SBN.random("ABCDE")
-        tree_dist = scd.tree_distribution()
-        pcsp_probs = scd.pcsp_probabilities()
+        sbn = SBN.random("ABCDE")
+        tree_dist = sbn.tree_distribution()
+        pcsp_probs = sbn.pcsp_probabilities()
         pcsp, prob = random.choice(list(pcsp_probs.items()))
         self.assertAlmostEqual(tree_dist.prob_all([pcsp.parent, pcsp.child]), prob)
 
     def test_probabilities(self):
-        scd = SBN.random("ABCDE")
-        tree_dist = scd.tree_distribution()
-        subsplit_probs, pcsp_probs = scd.probabilities()
+        sbn = SBN.random("ABCDE")
+        tree_dist = sbn.tree_distribution()
+        subsplit_probs, pcsp_probs = sbn.probabilities()
         subsplit, subsplit_prob = random.choice(list(subsplit_probs.items()))
         pcsp, pcsp_prob = random.choice(list(pcsp_probs.items()))
         self.assertAlmostEqual(tree_dist.feature_prob(subsplit), subsplit_prob)
@@ -177,11 +177,11 @@ class TestSBN(unittest.TestCase):
     def test_transit_probabilities(self):
         root_clade = Clade("ABCDE")
         root_subsplit = Subsplit(root_clade)
-        scd = SBN.random(root_clade)
-        tree_dist = scd.tree_distribution()
+        sbn = SBN.random(root_clade)
+        tree_dist = sbn.tree_distribution()
 
-        t_probs = scd.transit_probabilities()
-        s_probs = scd.subsplit_probabilities()
+        t_probs = sbn.transit_probabilities()
+        s_probs = sbn.subsplit_probabilities()
 
         parent = Subsplit(Clade("ABD"), Clade("C"))
         self.assertAlmostEqual(s_probs[parent], t_probs[parent][root_subsplit])
@@ -213,112 +213,112 @@ class TestSBN(unittest.TestCase):
             PCSP(Subsplit("A", "BC"), Subsplit("B", "C")): 1.0,
             PCSP(Subsplit("AB", "CD"), Subsplit("C", "D")): 1.0,
         }
-        scd1 = SBN(pcsp_conditional_probs1)
-        scd1.normalize()
-        dist1 = scd1.tree_distribution()
-        scd2 = SBN(pcsp_conditional_probs2)
-        scd2.normalize()
-        dist2 = scd2.tree_distribution()
+        sbn1 = SBN(pcsp_conditional_probs1)
+        sbn1.normalize()
+        dist1 = sbn1.tree_distribution()
+        sbn2 = SBN(pcsp_conditional_probs2)
+        sbn2.normalize()
+        dist2 = sbn2.tree_distribution()
 
         self.assertAlmostEqual(dist1.kl_divergence(dist2), 0.05411532090976828)
-        self.assertAlmostEqual(scd1.kl_divergence(scd2), 0.05411532090976828)
-        self.assertAlmostEqual(dist1.kl_divergence(scd2), 0.05411532090976828)
-        self.assertAlmostEqual(scd1.kl_divergence(dist2), 0.05411532090976828)
+        self.assertAlmostEqual(sbn1.kl_divergence(sbn2), 0.05411532090976828)
+        self.assertAlmostEqual(dist1.kl_divergence(sbn2), 0.05411532090976828)
+        self.assertAlmostEqual(sbn1.kl_divergence(dist2), 0.05411532090976828)
 
     def test_kl_random(self):
         taxa = Clade("ABCDE")
-        scd1 = SBN.random(taxa)
-        scd2 = SBN.random(taxa)
-        dist1 = scd1.tree_distribution()
-        dist2 = scd2.tree_distribution()
+        sbn1 = SBN.random(taxa)
+        sbn2 = SBN.random(taxa)
+        dist1 = sbn1.tree_distribution()
+        dist2 = sbn2.tree_distribution()
         dist_dist = dist1.kl_divergence(dist2)
-        scd_dist = scd1.kl_divergence(dist2)
-        scd_scd = scd1.kl_divergence(scd2)
-        dist_scd = dist1.kl_divergence(scd2)
-        self.assertAlmostEqual(dist_dist, scd_dist)
-        self.assertAlmostEqual(scd_dist, scd_scd)
-        self.assertAlmostEqual(scd_scd, dist_scd)
-        self.assertAlmostEqual(dist_scd, dist_dist)
+        sbn_dist = sbn1.kl_divergence(dist2)
+        sbn_sbn = sbn1.kl_divergence(sbn2)
+        dist_sbn = dist1.kl_divergence(sbn2)
+        self.assertAlmostEqual(dist_dist, sbn_dist)
+        self.assertAlmostEqual(sbn_dist, sbn_sbn)
+        self.assertAlmostEqual(sbn_sbn, dist_sbn)
+        self.assertAlmostEqual(dist_sbn, dist_dist)
 
     def test_restriction_identity(self):
         root_clade = Clade("ABCDEF")
         restriction = Clade("ABCDE")
 
-        scd = SBN.random_with_sparsity(root_clade, sparsity=0.5)
-        dist = scd.tree_distribution()
+        sbn = SBN.random_with_sparsity(root_clade, sparsity=0.5)
+        dist = sbn.tree_distribution()
         dist_res = dist.restrict(restriction)
 
-        scd_res = scd.restrict(restriction)
-        scd_res_dist = scd_res.tree_distribution()
+        sbn_res = sbn.restrict(restriction)
+        sbn_res_dist = sbn_res.tree_distribution()
 
-        scd_res_dist_scd = SBN.from_tree_distribution(scd_res_dist)
+        sbn_res_dist_sbn = SBN.from_tree_distribution(sbn_res_dist)
 
-        self.assertAlmostEqual(scd_res.kl_divergence(scd_res_dist_scd), 0.0)
-        self.assertAlmostEqual(scd_res_dist_scd.kl_divergence(scd_res), 0.0)
+        self.assertAlmostEqual(sbn_res.kl_divergence(sbn_res_dist_sbn), 0.0)
+        self.assertAlmostEqual(sbn_res_dist_sbn.kl_divergence(sbn_res), 0.0)
 
-        dist_res_scd = SBN.from_tree_distribution(dist_res)
+        dist_res_sbn = SBN.from_tree_distribution(dist_res)
 
-        self.assertAlmostEqual(dist_res_scd.kl_divergence(scd_res), 0.0)
-        self.assertAlmostEqual(scd_res.kl_divergence(dist_res_scd), 0.0)
+        self.assertAlmostEqual(dist_res_sbn.kl_divergence(sbn_res), 0.0)
+        self.assertAlmostEqual(sbn_res.kl_divergence(dist_res_sbn), 0.0)
 
-        dist_res_scd_dist = dist_res_scd.tree_distribution()
+        dist_res_sbn_dist = dist_res_sbn.tree_distribution()
 
-        self.assertAlmostEqual(scd_res_dist.kl_divergence(dist_res_scd_dist), 0.0)
-        self.assertAlmostEqual(dist_res_scd_dist.kl_divergence(scd_res_dist), 0.0)
+        self.assertAlmostEqual(sbn_res_dist.kl_divergence(dist_res_sbn_dist), 0.0)
+        self.assertAlmostEqual(dist_res_sbn_dist.kl_divergence(sbn_res_dist), 0.0)
 
     def test_restriction_subsplit_probs(self):
         root_clade = Clade("ABCDE")
         restriction = Clade("ABCD")
 
-        scd = SBN.random(root_clade)
-        dist = scd.tree_distribution()
+        sbn = SBN.random(root_clade)
+        dist = sbn.tree_distribution()
         dist_res = dist.restrict(restriction)
 
-        scd_res = scd.restrict(restriction)
+        sbn_res = sbn.restrict(restriction)
 
-        scd_res_ssprobs = scd_res.subsplit_probabilities()
+        sbn_res_ssprobs = sbn_res.subsplit_probabilities()
 
-        for ss, prob in scd_res_ssprobs.items():
+        for ss, prob in sbn_res_ssprobs.items():
             self.assertAlmostEqual(prob, dist_res.feature_prob(ss))
 
     def test_restriction_pcsp_probs(self):
         root_clade = Clade("ABCDE")
         restriction = Clade("ABCD")
 
-        scd = SBN.random(root_clade)
-        dist = scd.tree_distribution()
+        sbn = SBN.random(root_clade)
+        dist = sbn.tree_distribution()
         dist_res = dist.restrict(restriction)
 
-        scd_res = scd.restrict(restriction)
+        sbn_res = sbn.restrict(restriction)
 
-        scd_res_pcspprobs = scd_res.pcsp_probabilities()
+        sbn_res_pcspprobs = sbn_res.pcsp_probabilities()
 
-        for pcsp, prob in scd_res_pcspprobs.items():
+        for pcsp, prob in sbn_res_pcspprobs.items():
             self.assertAlmostEqual(prob, dist_res.feature_prob(pcsp))
 
     def test_max_prob_tree(self):
         root_clade = Clade("ABCDE")
-        scd = SBN.random(root_clade)
-        tree_dist = scd.tree_distribution()
+        sbn = SBN.random(root_clade)
+        tree_dist = sbn.tree_distribution()
 
         best_tree = tree_dist.max_item()
         best_lik = tree_dist.max_likelihood()
-        scd_best_tree, scd_best_lik = scd.max_prob_tree()
-        self.assertEqual(best_tree, scd_best_tree)
-        self.assertAlmostEqual(best_lik, scd_best_lik)
+        sbn_best_tree, sbn_best_lik = sbn.max_prob_tree()
+        self.assertEqual(best_tree, sbn_best_tree)
+        self.assertAlmostEqual(best_lik, sbn_best_lik)
 
     def test_restriction_conditional_subsplit_probs(self):
         root_clade = Clade("ABCDE")
         restriction = Clade("ABCD")
 
-        scd = SBN.random(root_clade)
-        dist = scd.tree_distribution()
+        sbn = SBN.random(root_clade)
+        dist = sbn.tree_distribution()
         dist_res = dist.restrict(restriction)
 
-        scd_res = scd.restrict(restriction)
+        sbn_res = sbn.restrict(restriction)
 
-        for pcsp in scd_res.iter_pcsp():
-            cond_prob = scd_res.get(pcsp)
+        for pcsp in sbn_res.iter_pcsp():
+            cond_prob = sbn_res.get(pcsp)
             joint_prob = dist_res.feature_prob(pcsp)
             parent_prob = dist_res.feature_prob(pcsp.parent)
             self.assertAlmostEqual(cond_prob, joint_prob/parent_prob)
@@ -334,21 +334,21 @@ class TestSBN(unittest.TestCase):
             PCSP(Subsplit("A", "BC"), Subsplit("B", "C")): 1.0,
             PCSP(Subsplit("AB", "CD"), Subsplit("C", "D")): 1.0,
         }
-        scd = SBN(pcsp_conditional_probs)
-        scd.normalize()
-        tree_dist = scd.tree_distribution()
-        clade_probs = scd.clade_probabilities()
+        sbn = SBN(pcsp_conditional_probs)
+        sbn.normalize()
+        tree_dist = sbn.tree_distribution()
+        clade_probs = sbn.clade_probabilities()
         max_clade_cred_tree = max(tree_dist, key=lambda tree: tree.clade_score(clade_probs))
-        max_clade_tree, max_clade_score = scd.max_clade_tree()
+        max_clade_tree, max_clade_score = sbn.max_clade_tree()
         self.assertEqual(max_clade_cred_tree, max_clade_tree)
         self.assertAlmostEqual(max_clade_cred_tree.clade_score(clade_probs), max_clade_score)
 
     def test_max_clade_tree(self):
-        scd = SBN.random("ABCDE")
-        tree_dist = scd.tree_distribution()
-        clade_probs = scd.clade_probabilities()
+        sbn = SBN.random("ABCDE")
+        tree_dist = sbn.tree_distribution()
+        clade_probs = sbn.clade_probabilities()
         max_clade_cred_tree = max(tree_dist, key=lambda tree: tree.clade_score(clade_probs))
-        max_clade_tree, max_clade_score = scd.max_clade_tree()
+        max_clade_tree, max_clade_score = sbn.max_clade_tree()
         self.assertEqual(max_clade_cred_tree, max_clade_tree)
         self.assertAlmostEqual(max_clade_cred_tree.clade_score(clade_probs), max_clade_score)
 
@@ -417,10 +417,10 @@ class TestSBN(unittest.TestCase):
                  Subsplit(Clade({'C'}), Clade({'D'}))): 0.0
         }
 
-        scd_big = SBN(pcsp_conditional_probs_big)
-        scd_ref1 = SBN(pcsp_conditional_probs1)
-        scd_ref2 = SBN(pcsp_conditional_probs2)
-        result = scd_big.restricted_kl_gradient_multi([scd_ref1, scd_ref2])
+        sbn_big = SBN(pcsp_conditional_probs_big)
+        sbn_ref1 = SBN(pcsp_conditional_probs1)
+        sbn_ref2 = SBN(pcsp_conditional_probs2)
+        result = sbn_big.restricted_kl_gradient_multi([sbn_ref1, sbn_ref2])
         for parent_clade in result:
             self.assertAlmostEqual(result[parent_clade], answer[parent_clade])
 
